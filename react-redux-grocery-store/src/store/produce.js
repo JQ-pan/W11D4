d305e7d65a0d481dfcb012140cca60cc073bb86f
lorenzo <lorenzotijerina@gmail.com>
2023-01-26 15:31:52 -0500

    phase 2 complete

--- react-redux-grocery-store/src/store/produce.js
+++ react-redux-grocery-store/src/store/produce.js
@@ -1,24 +1,24 @@
-import { assertThrowStatement } from '@babel/types';
-import produceData from '../mockData/produce.json'
+// import { assertThrowStatement } from '@babel/types';
+import produceData from "../mockData/produce.json";
 
-const POPULATE = "produce/POPULATE"
+const POPULATE = "produce/POPULATE";
 
 export function populateProduce() {
-    return {
-        type: POPULATE,
-        produce: produceData
-    }
+  return {
+    type: POPULATE,
+    produce: produceData,
+  };
 }
 
 export default function produceReducer(state = {}, action) {
-    switch (action.type) {
-        case POPULATE:
-            const newState = {};
-            action.produce.forEach(element => {
-                newState[element.id] = element;
-            });
-            return newState;
-        default: 
-            return state;
-    }
+  switch (action.type) {
+    case POPULATE:
+      const newState = {};
+      action.produce.forEach((element) => {
+        newState[element.id] = element;
+      });
+      return newState;
+    default:
+      return state;
+  }
 }
