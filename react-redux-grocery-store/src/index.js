e8f093793f4a7b9c2b0d31538fc3080c251a52df
lorenzo <lorenzotijerina@gmail.com>
2023-01-26 14:21:07 -0500

    setup complete

--- react-redux-grocery-store/src/index.js
+++ react-redux-grocery-store/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+
+const store = configureStore();
 
 function Root() {
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
@@ -16,5 +22,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
